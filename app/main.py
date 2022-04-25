533b24c3db939f0b398c092ba61bd0dd5db1f126
Lewi Uberg <lewiuberg@icloud.com>
2022-04-25 14:42:02 +0200

    Make frontend

--- app/main.py
+++ app/main.py
@@ -1,20 +1,33 @@
-import base64
-import io
 import os
 import shutil
 from pathlib import Path
 
-import numpy as np
 from confprint import prefix_printer
-from fastapi import APIRouter, FastAPI, File, Request, UploadFile
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi import FastAPI, File, Form, Request, Response, UploadFile
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from PIL import Image
-from tensorflow import expand_dims
+from modules.model import predicts
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.python.keras.models import Functional, Sequential
 
+
+# import numpy as np
+# import uvicorn
+
+
+PROJECT_ROOT = Path(__file__).parent.parent
+
+CLASSES = ["normal", "lgg", "hgg"]
+MODELS_PATH = f"{PROJECT_ROOT}/models"
+WIDTH_HEIGHT = (224, 224)
+
+cnn_custom: Sequential = load_model(f"{MODELS_PATH}/cnn_custom_model.h5")
+cnn_vgg16: Functional = load_model(f"{MODELS_PATH}/cnn_vgg16_model.h5")
+cnn_mobilenet: Sequential = load_model(f"{MODELS_PATH}/cnn_mobilenet_model.h5")
+cnn_densenet121: Functional = load_model(
+    f"{MODELS_PATH}/cnn_densenet121_model.h5"
+)
 
 p_information = prefix_printer(
     "INFO",
@@ -23,16 +36,6 @@ p_information = prefix_printer(
     frame_right="",
     whitespace=4,
 )
-
-CLASSES = ["normal", "lgg", "hgg"]
-MODELS_PATH = (
-    "/Users/lewiuberg/Documents/Data Science/Projects/tumorclass.info/models/"
-)
-MODEL_NAME = "cnn_densenet121"
-IMAGE_WIDTH = 224
-IMAGE_HEIGHT = 224
-
-model = load_model(os.path.join(MODELS_PATH, f"{MODEL_NAME}_model.h5"))
 
 app = FastAPI(
     title="Tumor Classification",
@@ -50,43 +53,118 @@ app = FastAPI(
 project_root = Path(__file__).parent.parent
 
 app.mount(
-    "/assets",
-    StaticFiles(directory=str(project_root / "assets")),
-    name="assets",
+    "/static",
+    StaticFiles(directory=str(project_root / "static")),
+    name="static",
 )
 
 templates = Jinja2Templates(directory=str(project_root / "templates"))
-app.mount("/assets", StaticFiles(directory="assets"), name="assets")
-
-# upload image file to memory
-@app.post("/predict")
-def predict(file: UploadFile = File(...)):
-    # Read image file as PIL image
-    img = Image.open(io.BytesIO(file.file.read()))
-    # If image is not RGB, convert to RGB
-    if img.mode != "RGB":
-        img = img.convert("RGB")
-    # Resize image to 224x224 and interpolate with nearest neighbour
-    if img.size != (IMAGE_WIDTH, IMAGE_HEIGHT):
-        img = img.resize((IMAGE_WIDTH, IMAGE_HEIGHT), Image.NEAREST)
-    # Convert image to array
-    img_array = img_to_array(img)
-    # Expand dimensions to create batch axis
-    img_array = expand_dims(img_array, 0)
-    # Predict
-    predictions = model.predict(img_array)
-
-    return {
-        "classification": CLASSES[np.argmax(predictions[0])].upper(),
-        "confidence": f"{np.max(predictions[0])*100:.2f}%",
-        # # use the CLASSES
-        CLASSES[0]: f"{predictions[0][0]*100:.2f}%",
-        CLASSES[1]: f"{predictions[0][1]*100:.2f}%",
-        CLASSES[2]: f"{predictions[0][2]*100:.2f}%",
-    }
 
 
-# use index.html as the homepage
+# get index page
 @app.get("/", response_class=HTMLResponse)
-def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+def get_home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+@app.get("/about", response_class=HTMLResponse)
+def get_about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
+
+
+@app.get("/data", response_class=HTMLResponse)
+def get_data(request: Request):
+    return templates.TemplateResponse("data.html", {"request": request})
+
+
+@app.get("/literature-review", response_class=HTMLResponse)
+def get_literature_review(request: Request):
+    # return StaticFiles
+    return Response(
+        content=open(
+            f"{PROJECT_ROOT}/static/html/literature-review.html", "rb"
+        ).read(),
+        media_type="text/html",
+    )
+
+
+@app.get("/conference-paper", response_class=HTMLResponse)
+def get_conference_paper(request: Request):
+    return "/static/html/literature-review.html"
+
+
+@app.post("/upload")
+def upload_file(request: Request, file: UploadFile = File(...)):
+    p_information("Uploading file...")
+    # get the file.filename extension and rename it to f'image.{extension}
+    # file.filename = f"image.{file.filename.split('.')[-1]}"
+    filename = file.filename
+    # use static mount to store the file in the temp folder
+    file_path = str(project_root / "static" / "temp" / filename)
+    # file_path = Path(project_root / "temp" / filename)
+    with open(file_path, "wb") as f:
+        f.write(file.file.read())
+    return templates.TemplateResponse(
+        "home.html", {"request": request, "filename": filename}
+    )
+
+
+@app.post("/predict")
+def prediction(
+    request: Request,
+    filename: str = Form(...),
+    custom_model: bool = Form(False),
+    vgg16_model: bool = Form(False),
+    mobilenet_model: bool = Form(False),
+    densenet121_model: bool = Form(False),
+):
+    selected_models = {}
+    if custom_model:
+        selected_models["cnn_custom"] = cnn_custom
+    if vgg16_model:
+        selected_models["cnn_vgg16"] = cnn_vgg16
+    if mobilenet_model:
+        selected_models["cnn_mobilenet"] = cnn_mobilenet
+    if densenet121_model:
+        selected_models["cnn_densenet121"] = cnn_densenet121
+    # get filename from the request
+    predictions = predicts(
+        path=f"{PROJECT_ROOT}/static/temp/{filename}",
+        target_size=WIDTH_HEIGHT,
+        models=selected_models,
+        classes=CLASSES,
+    )
+    return templates.TemplateResponse(
+        "home.html",
+        {
+            "request": request,
+            "filename": filename,
+            "predictions": predictions,
+            "classes": CLASSES,
+            "custom_model": custom_model,
+            "vgg16_model": vgg16_model,
+            "mobilenet_model": mobilenet_model,
+            "densenet121_model": densenet121_model,
+        },
+    )
+
+
+@app.on_event("startup")
+async def startup():
+    p_information("Starting app...")
+    if not os.path.exists(project_root / "static" / "temp"):
+        os.mkdir(project_root / "static" / "temp")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    p_information("Shutting down...")
+    shutil.rmtree(project_root / "static" / "temp")
+
+
+# TODO: Submit 2 supervisor reports, one for today, and one for Friday.
+# TODO: Make a Flowchart of the custom CNN.
+# TODO: Make diagram of architecture(s).
+# TODO: Finalise the "webpage" <- Printscreens
+# TODO: Expand on the Lit. Rev. by looking at the guidelines to make the thesis
+# TODO: Look for more testing data.
---
+++ app/modules/model.py
@@ -0,0 +1,107 @@
+from pathlib import Path
+from typing import Union
+
+import numpy as np
+from tensorflow import expand_dims
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.python.keras.models import Functional, Sequential
+
+
+PROJECT_ROOT = Path(__file__).parent.parent
+
+CLASSES = ["normal", "lgg", "hgg"]
+MODELS_PATH = f"{PROJECT_ROOT}/models"
+WIDTH_HEIGHT = (224, 224)
+
+
+def predict(
+    model: Union[Sequential, Functional],
+    model_name: str,
+    path: str = MODELS_PATH,
+    target_size: tuple = WIDTH_HEIGHT,
+    classes: list = CLASSES,
+) -> dict:
+    """
+    Predict the class of an image.
+
+    Parameters
+    ----------
+    model : Union[Sequential, Functional]
+        The model to use for prediction.
+    model_name : str
+        The name of the model.
+    path : str
+        Path to the image. Default: MODELS_PATH
+    target_size : tuple
+        The target size of the image. Default: WIDTH_HEIGHT
+    classes : list
+        The list of classes. Default: CLASSES
+
+    Returns
+    -------
+    dict
+        The prediction of the model.
+    """
+    # load the image
+    image = load_img(path=path, target_size=target_size)
+    # convert the image to a numpy array
+    image = img_to_array(image)
+    # expand the shape of the array to include the batch dimension
+    image = expand_dims(image, axis=0)
+    # predict the class of the image
+    prediction = model.predict(image)
+
+    return {
+        "model_name": model_name,
+        "classification": classes[np.argmax(prediction[0])].upper(),
+        "confidence": f"{np.max(prediction[0])*100:.2f}%",
+        classes[0]: f"{prediction[0][0]*100:.2f}%",
+        classes[1]: f"{prediction[0][1]*100:.2f}%",
+        classes[2]: f"{prediction[0][2]*100:.2f}%",
+    }
+
+
+def predicts(
+    models: dict,
+    path: str = MODELS_PATH,
+    target_size: tuple = WIDTH_HEIGHT,
+    classes: list = CLASSES,
+    line_format: bool = False,
+) -> dict:
+    """
+    Predict the class of an image.
+
+    Parameters
+    ----------
+    models : dict
+        The models to use for prediction.
+    path : str
+        Path to the image.
+    target_size : tuple
+        The target size of the image.
+    classes : list
+        The list of classes.
+
+    Returns
+    -------
+    dict
+        The prediction of the model.
+    """
+    predictions = {}
+    for model_name, model in models.items():
+        temp_prediction = predict(
+            model=model,
+            model_name=model_name,
+            path=path,
+            target_size=target_size,
+            classes=classes,
+        )
+
+        del temp_prediction["model_name"]
+
+        predictions[model_name] = temp_prediction
+
+    if line_format:
+        return format_predictions(predictions=predictions)
+    else:
+        return predictions
